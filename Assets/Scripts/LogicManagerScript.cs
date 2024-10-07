abf5c13c475a03b37cac13319b310cacd24e7927
sean842 <seanbleier123@gmail.com>
2024-10-07 22:17:34 +0300

    Finish Character Selection Logic

--- Assets/Scripts/LogicManagerScript.cs
+++ Assets/Scripts/LogicManagerScript.cs
@@ -10,11 +10,29 @@ public class LogicManagerScript : MonoBehaviour
     public int playerScore;
     public Text scoreText;
     public Text FinelScore;
+    //public Sprite Bird;
+    public SpriteRenderer BirdRenderer;
 
+    [Header("CharacterSelection")]
+    public Image SelectedCharacterImage;
+    public Button[] characterButtons;
+    public Sprite[] characterSprites;
+
+    [Header("Canvas")]
     public GameObject gameOveScreen;
     public GameObject startScreen;
     public GameObject stage;
+    public GameObject settingsCanvas;
+    public GameObject menuCanvas;
 
+
+    private void Start() {
+        // Assign a listener to each button to handle the selection
+        for (int i = 0; i < characterButtons.Length; i++) {
+            int index = i;  // Local variable to avoid closure issues
+            characterButtons[i].onClick.AddListener(() => OnCharacterSelected(index));
+        }
+    }
 
     [ContextMenu("StartGame")]
     public void StartGame()
@@ -43,5 +61,23 @@ public class LogicManagerScript : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
+    public void GoToSettings() {
+        settingsCanvas.SetActive(true);
+        menuCanvas.SetActive(false);
+    }
 
+    public void GoToMenu() {
+        settingsCanvas.SetActive(false);
+        menuCanvas.SetActive(true);
+
+    }
+
+
+    public void OnCharacterSelected(int characterIndex) {
+        SelectedCharacterImage.sprite = characterSprites[characterIndex];
+        //Bird = characterSprites[characterIndex];
+        BirdRenderer.sprite = characterSprites[characterIndex];
+
+
+    }
 }
